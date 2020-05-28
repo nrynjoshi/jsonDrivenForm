cc5e85947ab63c58628b02ef899680bd57a0743b
prabeen-soti <prabeensoti@gmail.com>
2020-05-28 19:57:12 +0545

    remove all string and make constant

--- src/test/java/com/jsondriventemplate/app/TestData.java
+++ src/test/java/com/jsondriventemplate/app/TestData.java
@@ -1,19 +1,20 @@
 package com.jsondriventemplate.app;
 
-import com.jsondriventemplate.JSONTemplateConst;
+import com.jsondriventemplate.constant.JSONTemplateConst;
 import org.apache.commons.io.IOUtils;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class TestData {
 
     public static String readEmployeeJSON() throws IOException {
         File file = ResourceUtils.getFile("classpath:" + JSONTemplateConst.JSON_SCHEMA_ATTR + "employee.json");
         FileInputStream fisTargetFile = new FileInputStream(file);
-        return IOUtils.toString(fisTargetFile, "UTF-8");
+        return IOUtils.toString(fisTargetFile, StandardCharsets.UTF_8);
     }
 
 }
