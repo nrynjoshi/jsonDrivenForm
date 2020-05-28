562e6b96a83e7c9423c49e8bf0c7233c5b0b4af3
prabeen-soti <prabeensoti@gmail.com>
2020-05-28 19:58:54 +0545

    remove all string and make constant

--- src/main/java/com/jsondriventemplate/repo/DBConstant.java
+++ src/main/java/com/jsondriventemplate/repo/DBConstant.java
@@ -5,6 +5,4 @@ public final class DBConstant {
     public static final String TEMPLATE_INFORMATION="template_information";
     public static final String JSON_TEMPLATE_DEFINITION="json_template_definition";
     public static final String EMPLOYEE="employee";
-    public static final String USER="user";
-
 }
